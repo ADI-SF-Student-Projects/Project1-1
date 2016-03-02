4829a32db3221dbbbaa3ffe648e9ef5e11fff3ae
Michael B <michaelbor2015@gmail.com>
2016-03-01 21:26:41 -0800

    Added Intent

--- MyAwesome/app/src/main/java/com/mikhail/myawesometodolist/DetailActivity.java
+++ MyAwesome/app/src/main/java/com/mikhail/myawesometodolist/DetailActivity.java
@@ -1,13 +1,22 @@
 package com.mikhail.myawesometodolist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    TextView toDoListHeaderSecond;
+    EditText secondActivityEditText;
+    ListView secondActivityListView;
+    Intent mainActivityIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +24,11 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        toDoListHeaderSecond = (TextView) findViewById(R.id.header1);
+        secondActivityEditText = (EditText) findViewById(R.id.editText1);
+        secondActivityListView = (ListView) findViewById(R.id.list1);
+        mainActivityIntent = getIntent();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
--- MyAwesome/app/src/main/java/com/mikhail/myawesometodolist/MainActivity.java
+++ MyAwesome/app/src/main/java/com/mikhail/myawesometodolist/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mikhail.myawesometodolist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView toDoListHeaderFirst;
+    EditText firstActivityEditText;
+    ListView firstActivityListView;
+    Intent detailActivityTransition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        toDoListHeaderFirst = (TextView) findViewById(R.id.header);
+        firstActivityEditText = (EditText) findViewById(R.id.editText);
+        firstActivityListView = (ListView) findViewById(R.id.list);
+        detailActivityTransition = new Intent(this, DetailActivity.class);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,25 +43,34 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
+
+
+
+
+
+
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
